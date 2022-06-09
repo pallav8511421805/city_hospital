64bcd152474be52406cfc39bfdb0bd025f37a95e
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-09 18:25:50 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./containers/Home";
 
 function App() {
   return (
     <>
     <Header/>
+    <Home/>
+    <Footer/>
     </>
   );
 }
