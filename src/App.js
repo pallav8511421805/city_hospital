07aa54f0f20ec2856f2e0794a18e7abf7e75b14f
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-09 18:48:29 +0530

    chnage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./containers/Home";
 
 function App() {
   return (
     <>
     <Header/>
-
+    
     <Footer/>
     </>
   );
