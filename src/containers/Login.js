208db790bcf8ccb0af8f2b541c394fbe6ae89f11
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-13 16:37:01 +0530

    change

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,33 +2,31 @@ import React from 'react';
 
 function Login(props) {
     return (
-        <>
-            <div className="section-title">
-                <h2>Login</h2>
+  <div className="container">
+  <div className="section-title">
+      <h2>Contact</h2>
+    </div>
+    <div className="row mt-5">
+      <div className="col-lg-12 mt-5 mt-lg-0">
+        <div className="php-email-form">
+            <>
+          <div className="row justify-content-center">
+            <div className="col-md-6 form-group">
+              <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
             </div>
+            </div>
+             <div className="row justify-content-center">
+            <div className="col-md-6 form-group mt-3 mt-md-0">
+              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+            </div>
+          </div>
+          </>
+          <div className="text-center my-3"><button class="appointment-btn scrollto border-0">Log in</button></div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-            <div className="container">
-                <div className="col-lg-8 mt-5 mt-lg-0">
-                    <form action method="post" role="form" className="php-email-form">
-                        <div className="row">
-                            <div className="col-md-6 form-group">
-                                <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
-                            </div>
-                            <div className="col-md-6 form-group mt-3 mt-md-0">
-                                <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
-                            </div>
-                        </div>
-                        <div className="form-group mt-3">
-                            <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
-                        </div>
-                        <div className="form-group mt-3">
-                            <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
-                        </div>
-                        <div className="text-center"><button type="submit">Send Message</button></div>
-                    </form>
-                </div>
-            </div>
-        </>
     );
 }
 
