10371008ab061c88a0fa21b195c76a1ac85df1d5
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-13 17:06:47 +0530

    change login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(props) {
+    const [usertype,setusertype] = useState("Log in");
+
     return (
   <div className="container">
   <div className="section-title">
-      <h2>Contact</h2>
+      {
+        usertype === "Log in" ? <h2>Log in</h2> : <h2>Sign up</h2>
+      }
     </div>
     <div className="row mt-5">
       <div className="col-lg-12 mt-5 mt-lg-0">
         <div className="php-email-form">
             <>
-          <div className="row justify-content-center">
-            <div className="col-md-6 form-group">
-              <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
-            </div>
-            </div>
-             <div className="row justify-content-center">
-            <div className="col-md-6 form-group mt-3 mt-md-0">
-              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+            {
+                usertype === "Log in" ? null :
+                <div className="row justify-content-center">
+                <div className="col-md-6 form-group my-1">
+                  <input type="text" name="name" className="form-control shadow-none" id="name" placeholder="Your Name" required />
+                </div>
+                </div> 
+            }
+
+            <div className="row justify-content-center">
+            <div className="col-md-6 form-group my-1">
+              <input type="email" className="form-control shadow-none" name="email" id="email" placeholder="Your Email" required />
             </div>
           </div>
+            
+            <div className="row justify-content-center">
+            <div className="col-md-6 form-group my-1">
+              <input type="password" name="password" className="form-control shadow-none" id="password" placeholder="Your password" required />
+            </div>
+            </div>
+
           </>
-          <div className="text-center my-3"><button class="appointment-btn scrollto border-0">Log in</button></div>
+          {
+            usertype === "Log in" ? <div className="text-center my-3"><button className="appointment-btn scrollto border-0">Log in</button></div> : <div className="text-center my-3"><button className="appointment-btn scrollto border-0">Sign up</button></div> 
+          }
+          <div className="text-center">
+          {
+            usertype === "Log in" ? <p>Create a new account <span><button className="appointment-btn scrollto border-0 ms-0" onClick={()=>{setusertype("Sign up")}}>Log in</button></span></p> : <p>Already have an account? <span><button className="appointment-btn scrollto border-0 ms-0" onClick={()=>{setusertype("Log in")}}>Sign up</button></span></p>
+          }
+          </div>
         </div>
       </div>
     </div>
