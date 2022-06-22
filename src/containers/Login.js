141e8d6501c66a127a70de6a02507592bc9280ee
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-22 16:54:51 +0530

    change

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import * as yup from 'yup';
-import { Form, Formik, useFormik } from 'formik';
+import { Form, Formik, useFormik} from 'formik';
 
 function Login(props) {
   const [usertype, setusertype] = useState("Log in");
   const [reset, setreset] = useState(false);
 
   let schema = yup.object().shape({
-    email: yup.string().email().required(),
+    email: yup.string().email("Please enter your vaild email id.").required("Please enter your vaild email id."),
     password : yup.string().required()
   });
-
+     function handleSubmit(){
+      SignupForm();
+     }
+     function handleBlur(){
+      SignupForm();
+     }
+     function handleChange(){
+      SignupForm();
+     }
   const SignupForm = () => {
     const formik = useFormik({
       initialValues: {
@@ -18,12 +26,13 @@ function Login(props) {
         lastName: '',
         email: '',
       },
+      validationSchema : schema,
       onSubmit: values => {
         alert(JSON.stringify(values, null, 2));
       },
-    })
+    }
+    )
   }
-  let[errors,handleSubmit,handleChange,values] = formik;
   return (
     <div className="container">
       <div className="section-title">
@@ -35,7 +44,7 @@ function Login(props) {
       <div className="row mt-5">
         <div className="col-lg-12 mt-5 mt-lg-0">
           <Formik>
-          <Form onSubmit={handleSubmit} className="php-email-form">
+          <Form onSubmit={formik.handleSubmit} className="php-email-form">
             <>
               {
                 usertype === "Log in" ? null :
@@ -71,14 +80,19 @@ function Login(props) {
                     <>
                       <div className="row justify-content-center">
                         <div className="col-md-6 form-group my-1">
-                          <input type="email" className="form-control shadow-none" name="email" id="email" placeholder="Your Email" required />
+                          <input type="email" className="form-control shadow-none" name="email" id="email" placeholder="Your Email" 
+                          onChange={handleChange} 
+                          onBlur={handleBlur}/>
                         </div>
                       </div>
 
                       <div className="row justify-content-center">
                         <div className="col-md-6 form-group my-1">
-                          <input type="password" name="password" className="form-control shadow-none" id="password" placeholder="Your password" required />
+                          <input type="password" name="password" className="form-control shadow-none" id="password" placeholder="Your password" 
+                          onChange={handleChange} 
+                          onBlur={handleBlur}/>
                         </div>
+                        <p>{errors}</p>
                       </div>
                     </>
                     : null
@@ -93,7 +107,7 @@ function Login(props) {
 
             </>
             {
-              reset ? <div className="text-center my-3"><button className="appointment-btn scrollto border-0">Change password</button></div> : usertype === "Log in" ? <div className="text-center my-3"><button className="appointment-btn scrollto border-0">Log in</button></div> : <div className="text-center my-3"><button className="appointment-btn scrollto border-0">Sign up</button></div>
+              reset ? <div className="text-center my-3"><button className="appointment-btn scrollto border-0">Change password</button></div> : usertype === "Log in" ? <div className="text-center my-3"><button className="appointment-btn scrollto border-0" type='submit'>Log in</button></div> : <div className="text-center my-3"><button className="appointment-btn scrollto border-0">Sign up</button></div>
             }
             <div className="text-center">
               {
