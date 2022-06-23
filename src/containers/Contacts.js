431483431f115d856e08cb46c73b7e803d5a8b73
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-23 08:36:10 +0530

    change

--- src/containers/Contacts.js
+++ src/containers/Contacts.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import * as yup from 'yup';
+import { Form, Formik, useFormik } from 'formik';
 
 function Contacts(props) {
+
+  let schema = yup.object().shape({
+    message: yup.string().required("Please enter your message."),
+    subject: yup.string().required("Please enter your subject."),
+    name: yup.string().required("Please enter your name."),
+    email: yup.string().email("Please enter your vaild email id.").required("Please enter your email id."),        
+})
+
+const formik = useFormik({
+    initialValues: {
+        name: '',
+        email: '',
+        subject: '',
+    },
+    validationSchema: schema,
+    onSubmit: values => {
+        alert(JSON.stringify(values, null, 2));
+    },
+})
+let { errors, handleBlur, handleSubmit, handleChange , touched, values} = formik;
     return (
 <div>
   <main id="main">
@@ -35,28 +57,50 @@ function Contacts(props) {
             </div>
           </div>
           <div className="col-lg-8 mt-5 mt-lg-0">
-            <form action method="post" role="form" className="php-email-form">
+            <Formik validationSchema>
+            <Form onSubmit={handleSubmit} className="php-email-form">
               <div className="row">
                 <div className="col-md-6 form-group">
-                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                  <input type="text" 
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.name}
+                  name="name" className="form-control" id="name" placeholder="Your Name"/>
+                   {errors.name || touched.name ? <p className='text-center'>{errors.name}</p> : ""}
                 </div>
                 <div className="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                  <input type="email" 
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  className="form-control" name="email" id="email" placeholder="Your Email" />
+                  {errors.email && touched.email ? <p className='text-center'>{errors.email}</p> : ""}
                 </div>
               </div>
               <div className="form-group mt-3">
-                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+                <input type="text" 
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.subject}
+                className="form-control" name="subject" id="subject" placeholder="Subject"/>
+                {errors.subject && touched.subject ? <p className='text-center'>{errors.subject}</p> : ""}
               </div>
               <div className="form-group mt-3">
-                <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
+                <textarea className="form-control"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.message}
+                name="message" rows={5} placeholder="Message" defaultValue={""} />
+                {errors.message && touched.message ? <p className='text-center'>{errors.message}</p> : ""}
               </div>
               <div className="my-3">
                 <div className="loading">Loading</div>
                 <div className="error-message" />
                 <div className="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div className="text-center"><button type="submit">Send Message</button></div>
-            </form>
+              <div className="text-center"><button type='submit'>Send Message</button></div>
+            </Form>
+            </Formik>
           </div>
         </div>
       </div>
