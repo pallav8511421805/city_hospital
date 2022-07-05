693e23b9ad12df7839b5a1dead8e432a5878b996
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-05 14:15:51 +0530

    change

--- src/containers/appoiment/Listappoiment.js
+++ src/containers/appoiment/Listappoiment.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
 
 function Listappoiment(props) {
+    let booklocaldata = JSON.parse(localStorage.getItem("Appointment"));
+    const [data, setdata] = useState([])
+    useEffect(() => {
+        setdata(booklocaldata)
+    }, [])
     return (
-        <div>
-            
-        </div>
+        data.map((d, i) => {
+            return (
+                <div>
+                    <Card className='my-3 mx-3 col-3 border-3 border-primary'>
+                        <CardBody>
+                            <CardTitle tag="h5" className='text-primary'>
+                                {d.name}
+                            </CardTitle>
+                            <CardText>
+                                {d.date}
+                            </CardText>
+                            <Button color="primary" className='shadow-none mx-2 border-2 border-primary'>
+                                Edit
+                            </Button>
+                            <Button color="danger" className='shadow-none mx-2 border-2 border-danger'>
+                                Delete
+                            </Button>
+                        </CardBody>
+                    </Card>
+                </div>
+            )
+        })
     );
 }
 
