08176ebc0d010db80ba63f0fa9e6da1cfe6cf23a
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-11 13:27:19 +0530

    change

--- src/containers/routing/Privateroute.js
+++ src/containers/routing/Privateroute.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { Islogin } from './testlogin';
 
-function Privateroute(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function Privateroute({ component:Component , restricted=true , ...test }) {
+    if(Islogin && restricted){
+        <Route
+        {...test}
+        />    
+    } else{
+        <Component />
+    }
 }
 
 export default Privateroute;
--- src/containers/routing/Publicroute.js
+++ src/containers/routing/Publicroute.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { Islogin } from './testlogin';
 
-function Publicroute(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function Publicroute({ component:Component , restricted=true , ...test }) {
+        if(Islogin){
+            <Route
+            {...test}
+            />    
+        } else{
+            <Redirect to={"/H"}/>
+        }
 }
 
 export default Publicroute;
