561d6aa2542b47ce81583c93bdf95f4a5dc02607
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-19 12:56:03 +0530

    change

--- src/redux/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import counter_data from './reducer/counter.reducer'
-
-export default rootreducer = () => combineReducers({
-    counter: counter_data
-}) 
