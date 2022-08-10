edc9a4a470a4b66224f005e976467ae3b6c9bead
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-10 11:35:43 +0530

    change

--- src/context/Themecontext.js
+++ src/context/Themecontext.js
@@ -1,3 +1,8 @@
+import React, { createContext } from 'react';
+import { Themetoggle } from './Actiontypes';
+import { Themereducer } from './reducer/Theme.reducer';
 const initval ={
     theme : 'light'
-}
+} 
+
+export default ThemeContext;
