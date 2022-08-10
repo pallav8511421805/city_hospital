3b783bd09e9041cfbf72183b3ee0e2c8522868b8
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-10 16:59:13 +0530

    change

--- src/context/Themecontext.js
+++ src/context/Themecontext.js
@@ -1,7 +1,25 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 import { Themetoggle } from './Actiontypes';
 import { Themereducer } from './reducer/Theme.reducer';
-const initval ={
-    theme : 'light'
-} 
+const initval = {
+    theme: 'light'
+}
 export const ThemeContext = createContext();
+
+function Themeprovider({ children }) {
+
+    const [state, dispatch] = useReducer(Themereducer, initval);
+
+    const Toogle_Theme = (val) => {
+        let newtheme = val === 'light' ? 'dark' : 'light';
+        dispatch({ type: Themetoggle, payload: newtheme })
+    }
+
+    return (
+        <ThemeContext.Provider value={{ ...state, Toogle_Theme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
+export default ThemeContext;
