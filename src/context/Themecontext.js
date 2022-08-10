d441fe31960d0516081e5e1b83df0122cd42a938
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-10 11:17:01 +0530

    change

---
+++ src/context/Themecontext.js
@@ -0,0 +1,3 @@
+const initval ={
+    theme : 'light'
+}
---
+++ src/context/reducer/Theme.reducer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Themetoggle } from '../Actiontypes';
+
+export const Themereducer = (state = initval,action)=>{
+        switch(action.type){
+          case Themetoggle: 
+          return {
+            ...state,
+            theme : action.payload
+          }
+        }
+}
