f3f41b0d02b859e6dc5c3804bf268a73e3a6287f
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-10 11:17:35 +0530

    change

--- src/context/reducer/Theme.reducer.js
+++ src/context/reducer/Theme.reducer.js
@@ -8,5 +8,7 @@ export const Themereducer = (state = initval,action)=>{
             ...state,
             theme : action.payload
           }
+          default :
+          return state;
         }
 }
