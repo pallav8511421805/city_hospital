520d02a426970acda47d64e9ca911c6e4bdbfdb3
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-17 11:35:25 +0530

    change

--- src/redux/actions/signup.action.js
+++ src/redux/actions/signup.action.js
@@ -1,4 +1,4 @@
-import * as ActionTypes from '../../actiontypes';
+import * as ActionTypes from '../actiontypes';
 
 export const signupaction = (values) => (dispatch) =>{
 dispatch({type:ActionTypes.SIGN_UP,payload:values});
