570d326a934555d1f0e1712cf255cb39600e7b61
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-22 19:44:30 +0530

    change

--- src/Firebase.js
+++ src/Firebase.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8esD-fGp86mk9O15145tXXbRP4cUJlTQ",
-  authDomain: "cityhospital-973be.firebaseapp.com",
-  projectId: "cityhospital-973be",
-  storageBucket: "cityhospital-973be.appspot.com",
-  messagingSenderId: "725062703752",
-  appId: "1:725062703752:web:1281b0e3b715d90ce71d3d",
-  measurementId: "G-LTF906F0FF"
+    apiKey: "AIzaSyB8esD-fGp86mk9O15145tXXbRP4cUJlTQ",
+    authDomain: "cityhospital-973be.firebaseapp.com",
+    projectId: "cityhospital-973be",
+    storageBucket: "cityhospital-973be.appspot.com",
+    messagingSenderId: "725062703752",
+    appId: "1:725062703752:web:1281b0e3b715d90ce71d3d",
+    measurementId: "G-LTF906F0FF"
 };
 
 // Initialize Firebase
