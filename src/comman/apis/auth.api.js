0060f351b63a236d270a65246069a1086666c416
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-22 19:42:53 +0530

    change

--- src/comman/apis/auth.api.js
+++ src/comman/apis/auth.api.js
@@ -1,4 +1,23 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Firebase";
 
   export const signUpapi = (values)=>{
     console.log('signUpapi :=',values);
+
+    return new Promise((resolve,reject)=>{
+      createUserWithEmailAndPassword(auth, values.email, values.password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log("User : ",user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+          console.log("ErrorMessage : ",errorMessage);
+          console.log("ErrorCode : ",errorCode);
+        });
+    })
   }
