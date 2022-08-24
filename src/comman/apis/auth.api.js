8a3ad953d22d37d74280cc9080e5f4412fdf8871
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-24 16:49:56 +0530

    change

--- src/comman/apis/auth.api.js
+++ src/comman/apis/auth.api.js
@@ -10,13 +10,15 @@ export const signUpapi = (values) => {
         onAuthStateChanged(auth, (user) => {
           if (user) {
             sendEmailVerification(user)
-            .then((user)=>{
-              if (user.emailVerified) {
-                resolve({ payload: 'successfully' });
-              } else {
-                resolve({ payload: 'Please check your email' });
-              }
-            })
+          }
+        });
+      })
+      .then((user) => {
+        onAuthStateChanged(auth, (user) => {
+          if (user.emailVerified) {
+            resolve({ payload: 'successfully' });
+          } else {
+            resolve({ payload: 'Please check your email' });
           }
         });
       })
