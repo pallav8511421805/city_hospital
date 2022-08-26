dd55107596b3efa44a977d896fd7d1753cacbce4
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-26 12:23:52 +0530

    change

--- src/redux/saga/auth.saga.js
+++ src/redux/saga/auth.saga.js
@@ -6,11 +6,9 @@ import * as ActionTypes from '../actiontypes';
 function* signUp(action) {
   try {
     const user = yield call(signUpapi, action.payload);
-    console.log(user);
-    //   yield put({type: "USER_FETCH_SUCCEEDED", user: user});
+    yield put(setalertaction({ text: user.payload, color: 'success' }));
   } catch (e) {
-    console.log(e);
-    //   yield put({type: "USER_FETCH_FAILED", message: e.message});
+    yield put(setalertaction({ text: e.payload, color: 'error' }));
   }
 }
 function* signIn(action) {
