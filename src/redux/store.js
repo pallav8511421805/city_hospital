15b5813e62beaa184e46617947e764f81812eaa2
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-29 12:08:04 +0530

    change

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,22 @@ import createSagaMiddleware from 'redux-saga'
 import thunk from 'redux-thunk'
 import { Rootreducer } from './reducers/Rootreducer'
 import rootSaga from './saga/Rootsaga'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth']
+}
+const persistedReducer = persistReducer(persistConfig, Rootreducer)
+export let persistor = persistStore(store)
 
 const sagaMiddleware = createSagaMiddleware()
 
 const Middlewares = [sagaMiddleware, thunk];
 export const store = createStore(
-    Rootreducer,
+    persistedReducer,
     applyMiddleware(...Middlewares)
 )
 sagaMiddleware.run(rootSaga)
