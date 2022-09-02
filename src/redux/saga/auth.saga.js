099e95d714a793b15d7530b8b4a2d70b3582f562
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-02 12:50:22 +0530

    change

--- src/redux/saga/auth.saga.js
+++ src/redux/saga/auth.saga.js
@@ -1,66 +1,93 @@
-import { call, takeEvery, all, put } from "redux-saga/effects";
-import {signInapi, signingoogleapi, signoutapi, signUpapi } from "../../comman/apis/auth.api";
-import { historydata } from "../../History/history";
-import { setalertaction } from "../actions/alert.action";
-import {Logoutedaction, signedinaction} from "../actions/signup.action";
-import * as ActionTypes from "../actiontypes";
+import { call, takeEvery, all, put } from 'redux-saga/effects'
+import {
+  forgetapi,
+  signInapi,
+  signingoogleapi,
+  signoutapi,
+  signUpapi,
+} from '../../comman/apis/auth.api'
+import { historydata } from '../../History/history'
+import { setalertaction } from '../actions/alert.action'
+import {
+  forgetpasswordaction,
+  Logoutedaction,
+  signedinaction,
+} from '../actions/signup.action'
+import * as ActionTypes from '../actiontypes'
 
 function* signUp(action) {
   try {
-    const user = yield call(signUpapi, action.payload);
-    yield put(setalertaction({ text: user.payload, color: "success" }));
+    const user = yield call(signUpapi, action.payload)
+    yield put(setalertaction({ text: user.payload, color: 'success' }))
   } catch (e) {
-    yield put(setalertaction({ text: e.payload, color: "error" }));
+    yield put(setalertaction({ text: e.payload, color: 'error' }))
   }
 }
 function* signIn(action) {
   try {
-    const user = yield call(signInapi, action.payload);
-    yield put(signedinaction());
-    yield put(
-      setalertaction({ text: "Login successfully.", color: "success" })
-    );
-    historydata.push("/H");
+    const user = yield call(signInapi, action.payload)
+    yield put(signedinaction())
+    yield put(setalertaction({ text: 'Login successfully.', color: 'success' }))
+    historydata.push('/H')
   } catch (e) {
-    yield put(setalertaction({ text: e.payload, color: "error" }));
+    yield put(setalertaction({ text: e.payload, color: 'error' }))
   }
 }
 
 function* signout() {
   try {
-    const user = yield call(signoutapi);
-    yield put(Logoutedaction());
-    yield put(setalertaction({ text: user.payload, color: "success" }));
-    historydata.push("/Login");
+    const user = yield call(signoutapi)
+    yield put(Logoutedaction())
+    yield put(setalertaction({ text: user.payload, color: 'success' }))
+    historydata.push('/Login')
   } catch (e) {
-    yield put(setalertaction({ text: e.payload, color: "error" }));
+    yield put(setalertaction({ text: e.payload, color: 'error' }))
   }
 }
 
-function* signGoogle(){
+function* signGoogle() {
   try {
-    const user = yield call(signingoogleapi);
-    yield put(signedinaction(user.payload));
-    historydata.push("/H");
-    yield put(setalertaction({ text: "Login successfully.", color: "success" }));
+    const user = yield call(signingoogleapi)
+    yield put(signedinaction(user.payload))
+    historydata.push('/H')
+    yield put(setalertaction({ text: 'Login successfully.', color: 'success' }))
   } catch (e) {
-    yield put(setalertaction({ text: e.payload, color: "error" }));
+    yield put(setalertaction({ text: e.payload, color: 'error' }))
+  }
+}
+
+function* forgetpasswordsaga() {
+  try {
+    const user = yield call(forgetapi)
+    yield put(forgetpasswordaction())
+    yield put(setalertaction({ text: user.payload, color: 'success' }))
+  } catch (e) {
+    yield put(setalertaction({ text: e.payload, color: 'error' }))
   }
 }
 
 function* WatchSignUp() {
-  yield takeEvery(ActionTypes.SIGN_UP, signUp);
+  yield takeEvery(ActionTypes.SIGN_UP, signUp)
 }
 function* WatchSignin() {
-  yield takeEvery(ActionTypes.SIGN_IN, signIn);
+  yield takeEvery(ActionTypes.SIGN_IN, signIn)
 }
 
 function* WatchSignout() {
-  yield takeEvery(ActionTypes.LOGOUT, signout);
+  yield takeEvery(ActionTypes.LOGOUT, signout)
 }
 function* WatchSignGoogle() {
-  yield takeEvery(ActionTypes.SIGN_INGOOGLE, signGoogle);
+  yield takeEvery(ActionTypes.SIGN_INGOOGLE, signGoogle)
+}
+function* Watchforget() {
+  yield takeEvery(ActionTypes.FORGET, forgetpasswordsaga)
 }
 export default function* authSaga() {
-  yield all([WatchSignUp(), WatchSignin(), WatchSignout(),WatchSignGoogle()]);
+  yield all([
+    WatchSignUp(),
+    WatchSignin(),
+    WatchSignout(),
+    WatchSignGoogle(),
+    Watchforget(),
+  ])
 }
