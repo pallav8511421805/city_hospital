8560ce11795656cd08efdb72acc2901d44d1f0f0
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-02 12:27:42 +0530

    change

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,199 +1,325 @@
-import React, { useState } from 'react';
-import * as yup from 'yup';
-import {Form, Formik, useFormik} from 'formik';
-import { useDispatch } from 'react-redux';
-import { Forgetpassword, signinaction, signingoogle, signupaction } from '../redux/actions/signup.action';
+import React, { useState } from 'react'
+import * as yup from 'yup'
+import { Form, Formik, useFormik } from 'formik'
+import { useDispatch } from 'react-redux'
+import {
+  Forgetpassword,
+  signinaction,
+  signingoogle,
+  signupaction,
+} from '../redux/actions/signup.action'
 
 function Login(props) {
-  const [usertype, setusertype] = useState("Log in");
-  const [reset, setreset] = useState(false);
-  const dispatch = useDispatch();
-      
-  let initialVal;
-  let mainschema;
-  if(usertype == "Log in"){
-    initialVal ={
-      password:'',
+  const [usertype, setusertype] = useState('Log in')
+  const [reset, setreset] = useState(false)
+  const dispatch = useDispatch()
+
+  let initialVal
+  let mainschema
+  if (usertype == 'Log in') {
+    initialVal = {
+      password: '',
       email: '',
     }
     mainschema = yup.object().shape({
-      email: yup.string().email("Please enter your vaild email id.").required("Please enter your email id."),
-      password: yup.string().required("Please enter your password.")
-    });
-  } else if(usertype == "Sign up"){
-    initialVal ={
+      email: yup
+        .string()
+        .email('Please enter your vaild email id.')
+        .required('Please enter your email id.'),
+      password: yup.string().required('Please enter your password.'),
+    })
+  } else if (usertype == 'Sign up') {
+    initialVal = {
       name: '',
-      password:'',
+      password: '',
       email: '',
     }
     mainschema = yup.object().shape({
-      name: yup.string().required("Please enter your name."),
-      email: yup.string().email("Please enter your vaild email id.").required("Please enter your email id."),
-      password: yup.string().required("Please enter your password.")
-    });
-  } else if (reset == true){
-    initialVal ={
-       email: '',
+      name: yup.string().required('Please enter your name.'),
+      email: yup
+        .string()
+        .email('Please enter your vaild email id.')
+        .required('Please enter your email id.'),
+      password: yup.string().required('Please enter your password.'),
+    })
+  } else if (reset == true) {
+    initialVal = {
+      email: '',
     }
     mainschema = yup.object().shape({
-      email: yup.string().email("Please enter your vaild email id.").required("Please enter your email id."),
-    });
+      email: yup
+        .string()
+        .email('Please enter your vaild email id.')
+        .required('Please enter your email id.'),
+    })
   }
 
-    let schema = mainschema;
+  let schema = mainschema
 
-    const handlelogin = (values)=>{
-      localStorage.setItem('user','123');
-    } 
-
-    const formik = useFormik({
-      initialValues: initialVal,
-     validationSchema : schema,  
-      onSubmit: values => {
-        if(usertype === "Log in"){
-          // handlelogin(values);
-          dispatch(signinaction(values))
-        }else if(reset){
-           handleforget(values)
-        }
-        else{
-          // alert(JSON.stringify(values, null, 2));
-          dispatch(signupaction(values))
-        }
-      },
-    }
-    )
-    let {errors,handleBlur,handleSubmit,touched,values ,handleChange} = formik;
-    const handlegoogle =() =>{
-     dispatch(signingoogle())
-    }
-    const handleforget = (values) =>{
-      console.log(values);
-      // dispatch(forgetpasswordaction())
-    }
+  const handlelogin = (values) => {
+    localStorage.setItem('user', '123')
+  }
+  const formik = useFormik({
+    initialValues: initialVal,
+    validationSchema: schema,
+    onSubmit: (values) => {
+      if (usertype === 'Log in') {
+        // handlelogin(values);
+        dispatch(signinaction(values))
+      } else if (usertype === 'Sign up') {
+        // alert(JSON.stringify(values, null, 2));
+        dispatch(signupaction(values))
+      } else {
+        dispatch(forgetpasswordaction())
+      }
+    },
+  })
+  let {
+    errors,
+    handleBlur,
+    handleSubmit,
+    touched,
+    values,
+    handleChange,
+  } = formik
+  const handlegoogle = () => {
+    dispatch(signingoogle())
+  }
   return (
     <div className="container">
       <div className="section-title">
-        {
-          reset ? <h2>Forget password</h2> : usertype === "Log in" ? <h2>Log in</h2> :
-            <h2>Sign up</h2>
-        }
+        {reset ? (
+          <h2>Forget password</h2>
+        ) : usertype === 'Log in' ? (
+          <h2>Log in</h2>
+        ) : (
+          <h2>Sign up</h2>
+        )}
       </div>
       <div className="row mt-5">
         <div className="col-lg-12 mt-5 mt-lg-0">
           <Formik validationSchema>
-          <Form onSubmit={handleSubmit} className="php-email-form">
-            <>
-              {
-                usertype === "Log in" ? null :
+            <Form onSubmit={handleSubmit} className="php-email-form">
+              <>
+                {usertype === 'Log in' ? null : (
                   <>
                     <div className="row justify-content-center">
                       <div className="col-md-6 form-group my-1">
-                        <input type="text" name="name" className="form-control shadow-none" id="name" placeholder="Your Name"
-                        onChange={handleChange} 
-                        onBlur={handleBlur}
-                        value={values.name}
-                        />
-                      </div>
-                      {errors.name || touched.name ?<p className='text-center'>{errors.name}</p>: ""}
-                    </div>
-                    <div className="row justify-content-center">
-                      <div className="col-md-6 form-group my-1">
-                        <input type="email" className="form-control shadow-none" name="email" id="email" placeholder="Your Email" 
-                        onChange={handleChange} 
-                        onBlur={handleBlur}
-                        value={values.email}
-                        />
-                      </div>
-                      {errors.email && touched.email ? <p className='text-center'>{errors.email}</p>: ""}
-                    </div>
-
-                    <div className="row justify-content-center">
-                      <div className="col-md-6 form-group my-1">
-                        <input type="password" name="password" className="form-control shadow-none" id="password" placeholder="Your password"
-                        onChange={handleChange} 
-                        onBlur={handleBlur}
-                        value={values.password}
-                        />
-                      </div>
-                      {errors.password && touched.password ? <p className='text-center'>{errors.password}</p>: ""}
-                    </div>
-                  </>
-              }
-
-              {
-                reset ?
-                  <div className="row justify-content-center">
-                    <div className="col-md-6 form-group my-1">
-                      <input type="email" className="form-control shadow-none" name="email" id="email" placeholder="Your Email"
-                      onChange={handleChange} 
-                      onBlur={handleBlur}
-                      value={values.email}
-                      />
-                    </div>
-                    {errors.email && touched.email ? <p className='text-center'>{errors.email}</p>: ""}
-                  </div>
-                  :
-                  usertype == "Log in" ?
-                    <>
-                      <div className="row justify-content-center">
-                        <div className="col-md-6 form-group my-1">
-                          <input type="email" className="form-control shadow-none" name="email" id="email" placeholder="Your Email" 
-                          onChange={handleChange} 
+                        <input
+                          type="text"
+                          name="name"
+                          className="form-control shadow-none"
+                          id="name"
+                          placeholder="Your Name"
+                          onChange={handleChange}
                           onBlur={handleBlur}
-                          value={values.email}/>
-                        </div>
-                        {errors.email && touched.email ? <p className='text-center'>{errors.email}</p>: ""}
+                          value={values.name}
+                        />
                       </div>
+                      {errors.name || touched.name ? (
+                        <p className="text-center">{errors.name}</p>
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                    <div className="row justify-content-center">
+                      <div className="col-md-6 form-group my-1">
+                        <input
+                          type="email"
+                          className="form-control shadow-none"
+                          name="email"
+                          id="email"
+                          placeholder="Your Email"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.email}
+                        />
+                      </div>
+                      {errors.email && touched.email ? (
+                        <p className="text-center">{errors.email}</p>
+                      ) : (
+                        ''
+                      )}
+                    </div>
 
-                      <div className="row justify-content-center">
-                        <div className="col-md-6 form-group my-1">
-                          <input type="password" name="password" className="form-control shadow-none" id="password" placeholder="Your password" 
-                          onChange={handleChange} 
+                    <div className="row justify-content-center">
+                      <div className="col-md-6 form-group my-1">
+                        <input
+                          type="password"
+                          name="password"
+                          className="form-control shadow-none"
+                          id="password"
+                          placeholder="Your password"
+                          onChange={handleChange}
                           onBlur={handleBlur}
                           value={values.password}
-                          />
-                        </div>
-                        {errors.password && touched.password ? <p className='text-center'>{errors.password}</p>: ""}
+                        />
                       </div>
-                    </>
-                    : null
-              }
+                      {errors.password && touched.password ? (
+                        <p className="text-center">{errors.password}</p>
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                  </>
+                )}
 
-            </>
-            {
-              reset ? <div className="text-center my-3"><button className="appointment-btn scrollto border-0" type='submit' >Change password</button></div> : usertype === "Log in" ? <div className="text-center my-3"><button className="appointment-btn scrollto border-0" type='submit'>Log in</button></div> : <div className="text-center my-3"><button className="appointment-btn scrollto border-0" type='submit'>Sign up</button></div>
-            }
-           <>
-           {
-              usertype === 'Log in' && reset === false ? <div className='text-center my-3'><button className='appointment-btn scrollto border-0' onClick={()=>handlegoogle()} type="button">Sign in With Google</button></div> : null
-            }
-             {
-                reset ? null : usertype === "Log in" ? <div className="text-center my-3">
-                  <button className="appointment-btn scrollto border-0 ms-0" onClick={() => {
-                    setreset(true);
-                  }}>Forgot password ?</button></div> : null
-              }
-           </>
-            <div className="text-center">
-              {
-                reset ? null : usertype === "Log in" ?
-                  <p>Create a new account <span>
-                      <a className="appointment-btn scrollto border-0 ms-0" onClick={() => { setusertype("Sign up"); setreset(false) }}>Log in</a>
-                  </span>
+                {reset ? (
+                  <div className="row justify-content-center">
+                    <div className="col-md-6 form-group my-1">
+                      <input
+                        type="email"
+                        className="form-control shadow-none"
+                        name="email"
+                        id="email"
+                        placeholder="Your Email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+                    </div>
+                    {errors.email && touched.email ? (
+                      <p className="text-center">{errors.email}</p>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                ) : usertype == 'Log in' ? (
+                  <>
+                    <div className="row justify-content-center">
+                      <div className="col-md-6 form-group my-1">
+                        <input
+                          type="email"
+                          className="form-control shadow-none"
+                          name="email"
+                          id="email"
+                          placeholder="Your Email"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.email}
+                        />
+                      </div>
+                      {errors.email && touched.email ? (
+                        <p className="text-center">{errors.email}</p>
+                      ) : (
+                        ''
+                      )}
+                    </div>
+
+                    <div className="row justify-content-center">
+                      <div className="col-md-6 form-group my-1">
+                        <input
+                          type="password"
+                          name="password"
+                          className="form-control shadow-none"
+                          id="password"
+                          placeholder="Your password"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.password}
+                        />
+                      </div>
+                      {errors.password && touched.password ? (
+                        <p className="text-center">{errors.password}</p>
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                  </>
+                ) : null}
+              </>
+              {reset ? (
+                <div className="text-center my-3">
+                  <button
+                    className="appointment-btn scrollto border-0"
+                    type="submit"
+                  >
+                    Change password
+                  </button>
+                </div>
+              ) : usertype === 'Log in' ? (
+                <div className="text-center my-3">
+                  <button
+                    className="appointment-btn scrollto border-0"
+                    type="submit"
+                  >
+                    Log in
+                  </button>
+                </div>
+              ) : (
+                <div className="text-center my-3">
+                  <button
+                    className="appointment-btn scrollto border-0"
+                    type="submit"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              )}
+              <>
+                {usertype === 'Log in' && reset === false ? (
+                  <div className="text-center my-3">
+                    <button
+                      className="appointment-btn scrollto border-0"
+                      onClick={() => handlegoogle()}
+                      type="button"
+                    >
+                      Sign in With Google
+                    </button>
+                  </div>
+                ) : null}
+                {reset ? null : usertype === 'Log in' ? (
+                  <div className="text-center my-3">
+                    <button
+                      className="appointment-btn scrollto border-0 ms-0"
+                      onClick={() => {
+                        setreset(true)
+                      }}
+                    >
+                      Forgot password ?
+                    </button>
+                  </div>
+                ) : null}
+              </>
+              <div className="text-center">
+                {reset ? null : usertype === 'Log in' ? (
+                  <p>
+                    Create a new account{' '}
+                    <span>
+                      <a
+                        className="appointment-btn scrollto border-0 ms-0"
+                        onClick={() => {
+                          setusertype('Sign up')
+                          setreset(false)
+                        }}
+                      >
+                        Log in
+                      </a>
+                    </span>
                   </p>
-                  : <p>Already have an account? <span>
-                    <a className="appointment-btn scrollto border-0 ms-0" onClick={() => { setusertype("Log in"); setreset(false) }}>Sign up</a>
-                  </span></p>
-              }
-            </div>
-          </Form>
+                ) : (
+                  <p>
+                    Already have an account?{' '}
+                    <span>
+                      <a
+                        className="appointment-btn scrollto border-0 ms-0"
+                        onClick={() => {
+                          setusertype('Log in')
+                          setreset(false)
+                        }}
+                      >
+                        Sign up
+                      </a>
+                    </span>
+                  </p>
+                )}
+              </div>
+            </Form>
           </Formik>
         </div>
       </div>
     </div>
-
-  );
+  )
 }
 
-export default Login;
+export default Login
--- src/redux/actions/signup.action.js
+++ src/redux/actions/signup.action.js
@@ -1,24 +1,28 @@
-import * as ActionTypes from "../actiontypes";
+import * as ActionTypes from '../actiontypes'
 
 export const signupaction = (values) => (dispatch) => {
-  dispatch({ type: ActionTypes.SIGN_UP, payload: values });
-};
+  dispatch({ type: ActionTypes.SIGN_UP, payload: values })
+}
 
 export const signinaction = (values) => (dispatch) => {
-  dispatch({ type: ActionTypes.SIGN_IN, payload: values });
-};
+  dispatch({ type: ActionTypes.SIGN_IN, payload: values })
+}
 
 export const signedinaction = (values) => (dispatch) => {
-  dispatch({ type: ActionTypes.SIGNED_IN, payload: values });
-};
+  dispatch({ type: ActionTypes.SIGNED_IN, payload: values })
+}
 
 export const Logoutaction = () => (dispatch) => {
-  dispatch({ type: ActionTypes.LOGOUT });
-};
+  dispatch({ type: ActionTypes.LOGOUT })
+}
 export const Logoutedaction = () => (dispatch) => {
-  dispatch({ type: ActionTypes.LOGOUTED });
-};
+  dispatch({ type: ActionTypes.LOGOUTED })
+}
 
-export const signingoogle =() => (dispatch)=>{
-  dispatch({type:ActionTypes.SIGN_INGOOGLE})
+export const signingoogle = () => (dispatch) => {
+  dispatch({ type: ActionTypes.SIGN_INGOOGLE })
+}
+
+export const forgetpasswordaction = (values) => (dispatch) => {
+  dispatch({ type: ActionTypes.FORGET, payload: values })
 }
